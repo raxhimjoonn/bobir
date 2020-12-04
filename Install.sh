cc3a1bede7e8f8a70688333867c04d091e2e5595
raxhimjoonn <74728819+raxhimjoonn@users.noreply.github.com>
2020-12-04 22:26:14 +0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,6 +1,7 @@
-cp webhack /data/data/com.termux/files/usr/bin/
+cp kamera ip /data/data/com.termux/files/usr/bin/
 cd /data/data/com.termux/files/usr/bin/
-chmod +777 webhack
+chmod +777 kamera ip
 cd $Home
 pip3 install requests
 clear
+
